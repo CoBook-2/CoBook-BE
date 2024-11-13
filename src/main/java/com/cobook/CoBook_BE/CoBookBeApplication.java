8ad67c02d5234db546b4e175367eb8f381845278
ChamsolCho <jocs5690@kyonggi.ac.kr>
2024-11-14 05:15:21 +0900

    2024.11.14 commit

---
+++ src/main/java/com/cobook/CoBook_BE/CoBookBeApplication.java
@@ -0,0 +1,13 @@
+package com.cobook.CoBook_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoBookBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoBookBeApplication.class, args);
+	}
+
+}
