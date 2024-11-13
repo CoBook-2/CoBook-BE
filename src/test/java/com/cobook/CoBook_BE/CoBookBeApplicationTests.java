a878397089c76f6cf87c9430eee15ec6ca15b80a
ChamsolCho <jocs5690@kyonggi.ac.kr>
2024-11-14 05:15:21 +0900

    2024.11.14 commit

---
+++ src/test/java/com/cobook/CoBook_BE/CoBookBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobook.CoBook_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoBookBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
