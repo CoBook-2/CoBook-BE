4c24c50f32adfc2a6a8a814ef806dec5277e1d62
ywhwe <esromer@kyonggi.ac.kr>
2024-11-21 11:05:43 +0900

    Added some annotation
    Annotated mongoDB related lines for while
    Tested within laptop

--- src/main/java/com/cobook/CoBook_BE/config/MongoDBConfig.java
+++ src/main/java/com/cobook/CoBook_BE/config/MongoDBConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.cobook.CoBook_BE.config;
 
 import org.springframework.context.annotation.Bean;
@@ -20,3 +21,4 @@ public class MongoDBConfig {
         return converter;
     }
 }
+*/
